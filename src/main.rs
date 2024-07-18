cec21d8a8dc6a0c5ce34e3da28624dac35786806
ARez2 <dark.gaming@fantasymail.de>
2024-07-18 21:28:15 +0200

    Make add_point a builder pattern

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 use macroquad::prelude::*;
 
 mod simulation;
-use simulation::{Simulation, Stick};
+use simulation::{Point, Simulation, Stick};
 
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
     let mut simulation = Simulation::new();
-    simulation.add_point(Vec2::new(screen_width() / 2.0, screen_height() / 2.0), 1.0, true);
-    simulation.add_point(Vec2::new(screen_width() / 2.0, screen_height() / 2.0 - 200.0), 1.0, false);
-    simulation.add_point(Vec2::new(screen_width() / 2.0 + 100.0, screen_height() / 2.0 - 200.0), 3.0, false);
-    simulation.add_stick(Stick::new(0, 1).min_length(50.0).max_length(200.0));
+    simulation.add_point(
+        Point::new(Vec2::new(screen_width() / 2.0, screen_height() / 2.0)).fixed()
+    );
+    simulation.add_point(Point::new(Vec2::new(screen_width() / 2.0, screen_height() / 2.0 - 200.0)));
+    simulation.add_point(Point::new(Vec2::new(screen_width() / 2.0 + 100.0, screen_height() / 2.0 - 200.0)).mass(3.0));
+    simulation.add_stick(Stick::new(0, 1).min_length(200.0).max_length(200.0));
     simulation.add_stick(Stick::new(1, 2).min_length(50.0).max_length(200.0));
     
     loop {
         clear_background(BLACK);
 
-        //let delta = macroquad::time::get_frame_time();
         let delta = 1.0/60.0;
         simulation.update(delta);
         simulation.draw();
--- src/simulation.rs
+++ src/simulation.rs
@@ -1,4 +1,5 @@
 use macroquad::prelude::*;
+use miniquad::window::screen_size;
 
 
 
@@ -18,6 +19,7 @@ impl Stick {
             max_length: f32::MAX
         }
     }
+
     pub fn min_length(mut self, val: f32) -> Self {
         self.min_length = val;
         self
@@ -28,15 +30,50 @@ impl Stick {
     }
 }
 
+// Only used for defining points, not in the Simulation itself
+#[derive(Debug)]
+pub struct Point {
+    position: Vec2,
+    fixed: bool,
+    mass: f32,
+    color: Color
+}
+impl Point {
+    pub fn new(position: Vec2) -> Self {
+        Self {
+            position,
+            mass: 1.0,
+            fixed: false,
+            color: WHITE
+        }
+    }
+
+    pub fn mass(mut self, val: f32) -> Self {
+        self.mass = val;
+        self
+    }
+    pub fn fixed(mut self) -> Self {
+        self.fixed = true;
+        self
+    }
+    pub fn color(mut self, val: Color) -> Self {
+        self.color = val;
+        self
+    }
+}
+
 
 #[derive(Debug)]
 pub struct Simulation {
     positions: Vec<Vec2>,
     prev_positions: Vec<Vec2>,
     masses: Vec<f32>,
+    colors: Vec<Color>,
     fixed: Vec<bool>,
     sticks: Vec<Stick>,
     force: Vec2,
+    // The index of a point to be dragged
+    drag_target: Option<usize>,
 }
 impl Simulation {
     pub fn new() -> Self {
@@ -44,17 +81,20 @@ impl Simulation {
             positions: vec![],
             prev_positions: vec![],
             masses: vec![],
+            colors: vec![],
             fixed: vec![],
             sticks: vec![],
             force: Vec2::new(0.0, 200.0),
+            drag_target: None
         }
     }
 
-    pub fn add_point(&mut self, position: Vec2, mass: f32, fixed: bool) {
-        self.positions.push(position);
-        self.prev_positions.push(position);
-        self.masses.push(mass);
-        self.fixed.push(fixed);
+    pub fn add_point(&mut self, point: Point) {
+        self.positions.push(point.position);
+        self.prev_positions.push(point.position);
+        self.masses.push(point.mass);
+        self.colors.push(point.color);
+        self.fixed.push(point.fixed);
     }
 
     pub fn add_stick(&mut self, stick: Stick) {
@@ -65,7 +105,23 @@ impl Simulation {
         if delta > 1.0 {
             return;
         }
+        let mouse_pos = mouse_position();
+        let mouse_pos = Vec2::new(mouse_pos.0, mouse_pos.1);
+        let pressed = is_mouse_button_pressed(MouseButton::Left);
+        if let Some(target) = self.drag_target {
+            if is_mouse_button_down(MouseButton::Left) {
+                self.positions[target] = Vec2::from(mouse_position());
+                self.prev_positions[target] = Vec2::from(mouse_position());
+            }
+        };
+        if is_mouse_button_released(MouseButton::Left) {
+            self.drag_target = None;
+        };
         for i in 0..self.positions.len() {
+            let radius: f32 = 7.0;
+            if pressed && mouse_pos.distance(self.positions[i]) < radius*2.0 {
+                self.drag_target = Some(i);
+            };
             if self.fixed[i] {
                 continue
             };
@@ -137,9 +193,16 @@ impl Simulation {
             let to = self.positions[self.sticks[i].to_idx];
             draw_line(from.x, from.y, to.x, to.y, 2.0, DARKGRAY);
         }
+
         for i in 0..self.positions.len() {
             let pos = self.positions[i];
-            draw_circle(pos.x, pos.y, 7.0, WHITE);
+            let color = if self.drag_target.is_some() && self.drag_target.unwrap() == i {
+                BLUE
+            } else {
+                self.colors[i]
+            };
+            
+            draw_circle(pos.x, pos.y, 7.0, color);
         }
     }
 }
