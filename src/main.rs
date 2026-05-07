e144758c7e460789083a825af48f0648317812a1
ARez2 <dark.gaming@fantasymail.de>
2026-05-07 12:49:09 +0200

    cleaned up code a bit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use std::time::{Duration, Instant};
 
-use macroquad::prelude::*;
+use macroquad::{miniquad::window::screen_size, prelude::*};
 
 mod simulation;
 use simulation::{IKChain, Link, Point, Simulation};
 
 pub mod ui;
-
-
 
 fn window_conf() -> Conf {
     Conf {
@@ -18,6 +16,79 @@ fn window_conf() -> Conf {
     }
 }
 
+fn setup_cloth(simulation: &mut Simulation) {
+    let width = 100;
+    let height = 22;
+    let spacing = Vec2::from(screen_size()) / Vec2::new(width as f32 + 1.0, height as f32 + 5.0);
+    let max_link_len = spacing.y;
+    for y in 0..height {
+        for x in 0..width {
+            let from_idx = y * width + x;
+            let mut pt = Point::new(spacing + Vec2::new(x as f32, y as f32) * spacing);
+            let stiff = 0.01;
+            let damp = 0.9;
+            if y == 0 {
+                pt = pt.fixed();
+            } else if y == height - 1 {
+                pt = pt.mass(5.0);
+            }
+            if y < height - 1 {
+                simulation.add_link(
+                    Link::new(from_idx, (y + 1) * width + x)
+                        .max_length(max_link_len)
+                        .stiffness(stiff)
+                        .damping(damp),
+                );
+            }
+            if x < width - 1 {
+                simulation.add_link(
+                    Link::new(from_idx, y * width + x + 1)
+                        .max_length(max_link_len)
+                        .stiffness(stiff)
+                        .damping(damp),
+                );
+            }
+            simulation.add_point(pt);
+        }
+    }
+}
+
+fn setup_IK(simulation: &mut Simulation) {
+    let chain_start_pos = Vec2::new(100.0, 500.0);
+    let chain_end_pos = Vec2::new(1800.0, 500.0);
+    let num_links = 20;
+    let diff = (chain_end_pos - chain_start_pos) / num_links as f32;
+    let link_length = diff.length();
+    simulation.add_point(Point::new(chain_start_pos).fixed());
+    for i in 1..=num_links {
+        simulation.add_point(Point::new(chain_start_pos + diff * i as f32));
+        simulation.add_link(
+            Link::new(i - 1, i)
+                .max_length(link_length)
+                .stiffness(0.01)
+                .damping(0.9),
+        );
+    }
+    simulation.add_ik_chain(IKChain::new((0..num_links).collect()));
+}
+
+enum TextPlacement {
+    Centered,
+    CenteredHorizontally,
+    TopLeftCorner,
+    AtPosition,
+}
+
+fn place_text(text: &str, x: f32, y: f32, placement: TextPlacement, color: Color, font_size: u16) {
+    let dims = measure_text(text, None, font_size, 1.0);
+    let position = match placement {
+        TextPlacement::CenteredHorizontally => (x - dims.width / 2.0, y + dims.height),
+        TextPlacement::TopLeftCorner => (x, y + dims.height),
+        TextPlacement::Centered => (x - dims.width / 2.0, y + dims.height / 2.0),
+        TextPlacement::AtPosition => (x, y),
+    };
+    draw_text(text, position.0, position.1, font_size as f32, color);
+}
 
 #[macroquad::main(window_conf)]
 async fn main() {
@@ -27,59 +98,42 @@ async fn main() {
     //rayon::ThreadPoolBuilder::new().num_threads(2).build_global().unwrap();
 
     let mut simulation = Simulation::new();
-    // let width = 100;
-    // let height = 22;
-    // let spacing = Vec2::from(screen_size()) / Vec2::new(width as f32 + 1.0, height as f32 + 5.0);
-    // let max_link_len = spacing.y;
-    // for y in 0..height {
-    //     for x in 0..width {
-    //         let from_idx = y * width + x;
-    //         let mut pt = Point::new(spacing + Vec2::new(x as f32, y as f32) * spacing);
-    //         let stiff = 0.01;
-    //         let damp = 0.9;
-    //         if y == 0 {
-    //             pt = pt.fixed();
-    //         } else if y == height-1 {
-    //             pt = pt.mass(5.0);
-    //         }
-    //         if y < height-1 {
-    //             simulation.add_link(Link::new(from_idx, (y+1) * width + x).max_length(max_link_len).stiffness(stiff).damping(damp));
-    //         }
-    //         if x < width-1 {
-    //             simulation.add_link(Link::new(from_idx, y * width + x+1).max_length(max_link_len).stiffness(stiff).damping(damp));
-    //         }
-    //         simulation.add_point(pt);
-    //     }
-    // }
-
-    let chain_start_pos = Vec2::new(100.0, 500.0);
-    let chain_end_pos = Vec2::new(1800.0, 500.0);
-    let num_links = 20;
-    let diff = (chain_end_pos - chain_start_pos) / num_links as f32;
-    let link_length = diff.length();
-    simulation.add_point(Point::new(chain_start_pos).fixed());
-    for i in 1..=num_links {
-        simulation.add_point(Point::new(chain_start_pos + diff * i as f32));
-        simulation.add_link(Link::new(i - 1, i).max_length(link_length).stiffness(0.01).damping(0.9));
-    }
-    simulation.add_ik_chain(IKChain::new((0..num_links).collect()));
+    setup_cloth(&mut simulation);
+    // setup_IK(&mut simulation);
 
     let mut time_sum = Duration::ZERO;
     let mut num_iterations = 0;
 
     loop {
         clear_background(BLACK);
+        place_text(
+            "Press SPACE to pause",
+            0.0,
+            0.0,
+            TextPlacement::TopLeftCorner,
+            GRAY,
+            40,
+        );
         if !simulation.paused {
             num_iterations += 1;
         }
         #[cfg(not(target_arch = "wasm32"))]
         if num_iterations > 0 {
-            let font_size: u16 = 40;
-            let text = format!("Avg. update time: {time:.*} ms", 3, time=(time_sum.as_millis() as f64 / num_iterations as f64));
-            let dims = measure_text(&text, None, font_size, 1.0);
-            draw_text(&text, screen_width()/2.0 - dims.width/2.0, dims.height, font_size as f32, GRAY);
+            let text = format!(
+                "Avg. update time: {time:.*} ms",
+                3,
+                time = (time_sum.as_millis() as f64 / num_iterations as f64)
+            );
+            place_text(
+                &text,
+                screen_width() / 2.0,
+                0.0,
+                TextPlacement::CenteredHorizontally,
+                GRAY,
+                40,
+            );
         }
-        
+
         #[cfg(not(target_arch = "wasm32"))]
         let start = Instant::now();
         simulation.update(1.0 / 180.0);
